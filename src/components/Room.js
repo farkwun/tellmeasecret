a44d0964d251c167d92b91f2af6233f21f1d74df
Bryan Chu <thisisbryanchu@gmail.com>
2018-03-12 12:02:45 -0400

    Creating Room component, adding styling

---
+++ src/components/Room.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Room extends Component {
+  render() {
+    return (
+      <div className="room">
+        <Person />
+      </div>
+    );
+  }
+}
+
+export default Room;
